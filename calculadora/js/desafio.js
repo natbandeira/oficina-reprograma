1f05d52b4911bbc37ea69df9990c322cd32744b1
natbandeira <nbandeira7@gmail.com>
2022-07-25 13:28:08 -0300

    changing how the answer is shown

--- calculadora/js/desafio.js
+++ calculadora/js/desafio.js
@@ -2,13 +2,14 @@
 
 // existe alguma forma de puxar a variável "valorHora" (dentro do escopo da função) do script.js e setar na variável "inputValorHora"? do desafio.js?
 let inputValorHora = document.querySelector("#valor-hora"); // atribuindo à variavel "inputValorHora", o elemento selecionado do HTML.
-let inputHorasProjeto = document.querySelector("#horas-projeto"); 
+let inputHorasProjeto = document.querySelector("#horas-projeto");
+let respostaFinal = document.querySelector("#resposta")
 
 function calculaValor(){
     let valorHora = inputValorHora.valueAsNumber; // atribuindo à variavel "valorHora", o valor do input como tipo Number (não aceita nenhuma letra, com exceção da "E" :S).
     let horasProjeto = inputHorasProjeto.valueAsNumber;
     let total = (valorHora * horasProjeto).toFixed(2); // atribuindo à variavel "total", a multiplicação das variáveis + método toFixed() para espicificar o número de casas decimais.
-    alert(`O valor deste projeto é: R$ ${total}`); // alert com template literals
+    respostaFinal.textContent = `O valor deste projeto é: R$ ${total}.`; // alert com template literals
 }
 
 // Apresentar o valor/hora que o usuário digitar do projeto; -ok
